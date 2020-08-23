172e1c3d99e8e88cf14664cfe2f0b8ab386af9aa
teppy90 <jared.tai90@gmail.com>
2020-08-23 20:39:55 +0800

    copied project 3's navbar, authservice, authcontext. next is creating search filter for videos

---
+++ src/Services/AuthServices.js
@@ -0,0 +1,66 @@
+const BACKEND_URL_USERS = process.env.REACT_APP_BACKEND_URL_USERS || 'http://localhost:3002/users';
+
+export default {
+    login: user => {
+        return fetch(`${BACKEND_URL_USERS}/login`, {
+            credentials: 'include',
+            method: "post",
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }).then(res => {
+            if (res.status !== 401) {
+                return res.json().then(data => {
+                    localStorage.setItem("access_token", data.user.token);
+                    return data
+                });
+            }
+            else {
+                return { isAuthenticated: false, user: { username: "" } };
+            }
+        })
+    },
+    register: user => {
+        return fetch(`${BACKEND_URL_USERS}/signup`, {
+            method: "post",
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }).then(res => res.json())
+            .then(data => data);
+    },
+    logout: () => {
+        return fetch(`${BACKEND_URL_USERS}/logout`, { credentials: 'include' })
+            .then(res => res.json())
+            .then(data => data)
+            .catch(err => {
+                // if no cookie, it will return error	
+                return { user: { username: "" }, success: true };
+            });
+    },
+    isAuthenticated: () => {
+        return fetch(`${BACKEND_URL_USERS}/authenticated`, { 
+            credentials: 'include',
+            headers: {
+                'access_token': localStorage.getItem('access_token')
+            }
+         })
+            .then(res => {
+                if (res.status !== 401) {
+                    return res.json().then(data => data);
+                }
+                else {
+                    return { isAuthenticated: false, user: { username: "" } };
+                }
+            })
+    },
+    findByID: (id) => {
+        return fetch(BACKEND_URL_USERS + '/' + id)
+            .then(response => response.json())
+            .then(data => data)
+    }
+}
