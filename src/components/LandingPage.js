4c4c074fdc87e157e5d90897f3fee9ec3bcb2de0
Shi Jie Teo <teoshijie@gmail.com>
2020-08-27 22:24:55 +0800

    edited landingpage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Facode } from "react-icons/fa";
-import { Card, Avatar, Col, Typography, Row } from 'antd';
+import { Avatar, Typography } from 'antd';
+import { Card, CardDeck, Container, Image, Col, Row } from 'react-bootstrap'
+
 import axios from "axios";
 import moment from "moment";
 const { Title } = Typography;
@@ -12,7 +14,7 @@ function LandingPage() {
     const [Videos, setVideos] = useState([])
 
     useEffect(() => {
-        axios.get('http://localhost3002/videos/getvideos')
+        axios.get('http://localhost:3002/videos')
             .then(response => {
                 if (response.data.success) {
                     console.log(response.data.videos)
@@ -28,37 +30,33 @@ function LandingPage() {
         let minutes = Math.floor(video.duration / 60)
         let seconds = Math.floor(video.duration - minutes * 60)
 
-        return <Col lg={6} md={8} xs={24}>
-            <div style={{ position: 'relative' }}>
-                <img style={{ width: '100%' }} alt="thumbnail" src={`http://localhost:3000/${video.thumbnail}`} />
-                <div className=" duration"
-                    style={{
-                        bottom: 0, right: 0, position: 'absolute', margin: '4px',
-                        color: '#fff', backgroundColor: 'rgba(17, 17, 17, 0.8)', opacity: 0.8,
-                        padding: '2px 4px', borderRadius: '2px', letterSpacing: '0.5px', fontSize: '12px',
-                        fontWeight: '500', lineHeight: '12px'
-                    }}>
-                    <span>{minutes} : {seconds}</span>
-                </div>
-            </div><br />
-            <Meta
-                avatar={
-                    <Avatar src={video.writer.image} />
-                }
-                title={video.title}
-            />
-            <span>{video.writer.name} </span><br />
-            <span style={{ marginLeft: '3rem' }}> {video.views}</span>
-    - <span> {moment(video.createdAt).format("MMM Do YY")} </span>
-        </Col>
+
+        return <CardDeck>
+        <Container>
+            <Row>
+                <Col>
+        <Card style={{ width: '16rem' }}>
+                  <Card.Img variant="top" src= {video.thumbnail} />
+                  <Card.Body>
+                    <Card.Title>
+                      <Image src = {video.writer.image} rounded style={{width:"10%"}} /> {video.title}         
+                    </Card.Title>
+                      <Card.Text>
+                        {video.writer.displayName} 
+                      </Card.Text>
+                  </Card.Body>
+              </Card>
+              </Col>
+              </Row>
+              </Container>
+              </CardDeck>
     })
 
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
             <Title level={2} > Recommended </Title>
-            <hr />
 
-            <Row>
+            <Row gutter={16}>
                 {renderCards}
             </Row>
         </div>
