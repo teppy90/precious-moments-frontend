ea32d4c96362e328b4182dc78111b1b8306956c9
Shi Jie Teo <teoshijie@gmail.com>
2020-09-03 20:23:10 +0800

    netflix style carousel

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { Facode } from "react-icons/fa";
 import { Card, Avatar, Col, Typography, Row } from 'antd';
-    import 'antd/dist/antd.css'
-
+import 'antd/dist/antd.css'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import axios from "axios";
 import moment from "moment";
+import '../components/landingpage.css'
+
 const { Title } = Typography;
 const { Meta } = Card
 
-
-function LandingPage() {
+function LandingPage(props) {
     const [Videos, setVideos] = useState([])
 
 
@@ -25,27 +27,47 @@ function LandingPage() {
             })
     }, [])
 
+    
+
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 4
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
+
+
+
     const renderCards = Videos.map((video, index) => {
 
 
-        return <Col lg={6} md={8} xs={24}>
-            <div style={{ position: 'relative' }}>
+        return (
+
+            <div className='item'>
+
                 <a href={`${video._id}/video`} >
-                        <video width="400" height="240"  src={video.video_url}/>
-            
-                    
+                    <video width="320" height="240" src={video.video_url} />
                 </a>
-            </div><br />
-            <Meta
-                avatar={
-                    <Avatar src={video.writer.image} />
-                }
-                title={video.writer.firstName + " " + video.writer.lastName} 
-            />
-            <span>{video.title}</span><br />
-            <span style={{ marginLeft: '3rem' }}> {video.views}</span>
-            <span> {moment(video.createdAt).format("Do MMM YYYY")} </span>
-        </Col>
+
+
+
+            </div>
+
+        )
 
     })
 
@@ -54,12 +76,32 @@ function LandingPage() {
 
     return (
 
-        <div style={{ width: '85%', margin: '3rem auto' }}>
+        <div style={{ width: '85%', margin: '2rem auto' }}>
             <Title level={2} > Recommended </Title>
+            <Carousel className='container'
+                swipeable={false}
+                draggable={false}
+                responsive={responsive}
+                ssr={true} // means to render carousel on server-side.
+                infinite={true}
+                keyBoardControl={true}
+                customTransition="all .5"
+                transitionDuration={500}
+                containerClass="carousel-container"
+                removeArrowOnDeviceType={["tablet", "mobile"]}
+                deviceType={props.deviceType}
+                dotListClass="custom-dot-list-style"
+                itemClass="carousel-item-padding-40-px"
+            >
 
-            <Row gutter={16}>
                 {renderCards}
-            </Row>
+
+
+            </Carousel>
+
+
+
+
         </div>
 
     )
---
+++ src/components/NetflixSlider/index.js
@@ -0,0 +1,6 @@
+import Slider from './Sliders'
+import Item from './item'
+
+Slider.Item = Item;
+
+export default Slider;
